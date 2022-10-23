3d07128c910c9e2a6a48feab3d9270f5787f33f6
Kittipong Laoaroon <kittipong.lao@ascendcorp.com>
2022-10-23 19:16:47 +0700

    fix: ui

--- lib/screens/destination_screen.dart
+++ lib/screens/destination_screen.dart
@@ -14,118 +14,141 @@ class DestinationScreen extends StatefulWidget {
   State<DestinationScreen> createState() => _DestinationScreenState();
 }
 
+const double fixedHeight = 400;
+
 class _DestinationScreenState extends State<DestinationScreen> {
+  ScrollController controller = ScrollController();
+  double coverImageHeight = fixedHeight;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(() {
+      setState(() {
+        double height = fixedHeight - controller.offset;
+        if (height >= 140) {
+          if (height >= 0) {
+            coverImageHeight = height;
+          } else {
+            coverImageHeight = 0;
+          }
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 400,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30),
+          SizedBox(
+            height: coverImageHeight,
+            child: Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: fixedHeight,
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                        blurRadius: 6,
+                      ),
+                    ],
                   ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0, 2),
-                      blurRadius: 6,
-                    )
-                  ],
-                ),
-                child: Hero(
-                  tag: widget.destination.imageUrl!,
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30),
-                    ),
-                    child: Image(
-                      image: AssetImage(widget.destination.imageUrl!),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-              SafeArea(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () => Navigator.pop(context),
-                      icon: const Icon(
-                        Icons.arrow_back,
-                        size: 30,
+                  child: Hero(
+                    tag: widget.destination.imageUrl!,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                      child: Image(
+                        image: AssetImage(widget.destination.imageUrl!),
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () => {},
-                          icon: const Icon(
-                            Icons.search,
-                            size: 30,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () => {},
-                          icon: const Icon(
-                            FontAwesomeIcons.sort,
-                            size: 25,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+                  ),
                 ),
-              ),
-              Positioned(
-                left: 20,
-                bottom: 20,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.destination.city!,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 1.2,
-                        fontSize: 33,
+                SafeArea(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          size: 30,
+                        ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          FontAwesomeIcons.locationArrow,
-                          size: 14,
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () => {},
+                            icon: const Icon(
+                              Icons.search,
+                              size: 30,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () => {},
+                            icon: const Icon(
+                              FontAwesomeIcons.bookmark,
+                              size: 25,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  bottom: 20,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.destination.city!,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
                           color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.2,
+                          fontSize: 33,
                         ),
-                        const SizedBox(width: 5),
-                        Text(
-                          widget.destination.country!,
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
-                            letterSpacing: 1.2,
-                            fontSize: 16,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            FontAwesomeIcons.locationArrow,
+                            size: 14,
+                            color: Colors.white,
                           ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
+                          const SizedBox(width: 5),
+                          Text(
+                            widget.destination.country!,
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              letterSpacing: 1.2,
+                              fontSize: 16,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
           Expanded(
             child: ListView.builder(
+              controller: controller,
               itemCount: widget.destination.activities!.length,
               padding: const EdgeInsets.only(top: 10, bottom: 10),
               itemBuilder: (context, index) {
@@ -150,8 +173,8 @@ class _DestinationScreenState extends State<DestinationScreen> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                SizedBox(
-                                  width: 160,
+                                Expanded(
+                                  // width: 120,
                                   child: Text(
                                     activity.name!,
                                     overflow: TextOverflow.ellipsis,
@@ -162,24 +185,27 @@ class _DestinationScreenState extends State<DestinationScreen> {
                                     ),
                                   ),
                                 ),
-                                Column(
-                                  children: [
-                                    Text(
-                                      "\$${activity.price!}",
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.w600,
-                                        fontSize: 20,
+                                SizedBox(
+                                  width: 60,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "\$${activity.price!}",
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 20,
+                                        ),
                                       ),
-                                    ),
-                                    Text(
-                                      "per pax",
-                                      style: TextStyle(
-                                        color: Colors.grey.shade400,
-                                        fontWeight: FontWeight.w600,
-                                        fontSize: 14,
+                                      Text(
+                                        "per pax",
+                                        style: TextStyle(
+                                          color: Colors.grey.shade400,
+                                          fontSize: 14,
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
